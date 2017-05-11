5535e98d7899681b3e002dca3feaff525fda4272
Zan Marolt <marolt.zan@hotmail.com>
2017-05-11 10:02:36 +0200

    Get data from firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire } from 'angularfire2'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  cousines = ['c1', 'c2', 'c3'];
+
+  constructor(af: AngularFire){
+    af.database.list('/cousines').subscribe(x => {
+      this.cousines = x;
+      console.log(this.cousines);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXivv71hDhDFBJ1GzSshf_j87r8XcRG7U",
+  authDomain: "pepper-9ed79.firebaseapp.com",
+  databaseURL: "https://pepper-9ed79.firebaseio.com",
+  projectId: "pepper-9ed79",
+  storageBucket: "pepper-9ed79.appspot.com",
+  messagingSenderId: "476098312415"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
